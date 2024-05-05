bc6abb50ba0b523e04701085bcc63732bc5f1055
sanil011 <sanilp191@gmail.com>
2024-05-05 16:22:40 +0530

    heading color change on hover

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import Section from '../layout/section'
 import { motion } from 'framer-motion';
 
-
 const Header = () => {
-  
     return (
         <div className='flex items-center gap-8 pt-28 '>
             <motion.div
@@ -16,8 +13,40 @@ const Header = () => {
                 </div>
             </motion.div>
             <div>
-                <h1 className='text-xl sm:text-3xl dark:text-main  text-black font-semibold font-poppin'>👋 Hi, I am Sanil</h1>
-                <h1 className='font-medium text-base sm:text-xl font-poppin text-lightText dark:text-text'>Frontend Software Engineer</h1>
+                <h1 className='text-xl sm:text-3xl dark:text-main cursor-pointer  text-black font-semibold font-poppin'>👋 Hi, I am Sanil</h1>
+                <motion.p
+                    whileHover={{ color: "orange" }}
+                    onMouseEnter={() => {
+                        const elem = document.getElementById("heading-post-animation");
+                        if (elem)
+                            elem.style.width = "100%"
+                    }}
+                    onMouseLeave={() => {
+                        const elem = document.getElementById("heading-post-animation");
+                        if (elem)
+                            elem.style.width = "0%"
+                    }}
+                    style={{
+                        position: "relative",
+                        overflow: "hidden",
+                    }}
+                >
+                    <motion.span
+                        id='heading-post-animation'
+                        style={{
+                            position: "absolute",
+                            bottom: 0,
+                            left: 0,
+                            width: 0,
+                            zIndex: 20,
+                            height: "2px",
+                            background: "orange",
+                            transition: "all",
+                            transitionDuration: "0.3s"
+                        }}
+                    />
+                    <h1 className='hover:dark:text-orange hover:text-orange cursor-pointer font-medium text-base sm:text-xl font-poppin text-lightText dark:text-text'>Frontend Software Engineer</h1>
+                </motion.p>
             </div>
         </div>
     )

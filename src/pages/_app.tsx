3f690f2627b6784fa07728e58d114b0ddfdc9648
sanil011 <sanilp191@gmail.com>
2024-05-12 15:53:15 +0530

    add vercel analyticS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@fontsource-variable/inter';
 import Navbar from "@/components/layout/navbar";
 import React from 'react';
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 // Supports weights 100-900
 import '@fontsource-variable/inter';
 import '@fontsource/poppins';
@@ -15,7 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider
         attribute="class"
         defaultTheme="dark"
-      >
+        >
+        <Analytics/>
       <Component {...pageProps} />
       </ThemeProvider>
     </React.Fragment>

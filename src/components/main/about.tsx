1bb41a92a73470f3cd367bf754eb43816e6c77d1
Sanil <105095685+sanil011@users.noreply.github.com>
2025-09-02 20:58:40 +0530

    sm

--- src/components/main/about.tsx
+++ src/components/main/about.tsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <Section>
             <h1 className='text-lightMain dark:text-main font-semibold leading-snug text-xl sm:text-2xl'>About.</h1>
-            <h1 className='text-lightText dark:text-text text-base lg:text-lg lg:leading-7'>I’m a Frontend Engineer with 2 years of experience, including a year as a full-time Software Engineer and a year as an intern. I’m skilled in React, Next.js, Typescript, and UI/UX, and have worked on building high-performance web apps while focusing on delivering smooth, user-friendly experiences.</h1>
+            <h1 className='text-lightText dark:text-text text-base lg:text-lg lg:leading-7'>I’m a Frontend Engineer with 2 years of experience, including as a full-time Software Engineer and as an intern. I’m skilled in React, Next.js, Typescript, and UI/UX, and have worked on building high-performance web apps while focusing on delivering smooth, user-friendly experiences.</h1>
         </Section>
     )
 }
